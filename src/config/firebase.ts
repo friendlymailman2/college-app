5b803c37a8b19179d5b7a8c6e2ba3d62743c7998
Adrien Launay <adrienlaunay@Adriens-MacBook-Air.local>
2023-08-23 20:26:18 -0700

    first commit

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvd3wOxGWOZqvRCcxsJbrh6c15pYCwkgY",
+  authDomain: "college-app-59394.firebaseapp.com",
+  projectId: "college-app-59394",
+  storageBucket: "college-app-59394.appspot.com",
+  messagingSenderId: "355498966731",
+  appId: "1:355498966731:web:d7728f9c07e2895a92e54e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
